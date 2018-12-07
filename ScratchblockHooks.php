227167debede2994cc59a688df4152c4b97dce0a
apple502j <apple502j@yahoo.co.jp>
2018-12-07 19:09:29 +0900

    update: scratchblocks to scratchblocks4

--- ScratchblockHooks.php
+++ ScratchblockHooks.php
@@ -2,32 +2,32 @@
 class Scratchblock4Hook {
 	// Ouput HTML for <scratchblocks> tag
 	
-	public static function sbParserInit (Parser $parser) {
+	public static function sb4ParserInit (Parser $parser) {
 		// Register <scratchblocks> and <sb> tag
-		$parser->setHook('scratchblocks4', array( "Scratchblock4Hook", 'sbRenderTag') );
-		$parser->setHook('sb4', array( "Scratchblock4Hook", 'sbRenderInlineTag') );
+		$parser->setHook('scratchblocks4', array( "Scratchblock4Hook", 'sb4RenderTag') );
+		$parser->setHook('sb4', array( "Scratchblock4Hook", 'sb4RenderInlineTag') );
 		//throw new Exception(var_dump($parser));
 		return true;
 	}
 	
-	public static function sbReadLS (array &$vars) {
+	public static function sb4ReadLS (array &$vars) {
 		global $wgScratchBlocks4Lang;
 		$vars['wgScratchBlocks4Lang'] = $wgScratchBlocks4Lang;
 		return true;
 	}
 	
-	public static function sbSetup() {
+	public static function sb4Setup() {
 		global $wgOut;
-		$wgOut->addModules('ext.scratchBlocks');
+		$wgOut->addModules('ext.scratchBlocks4');
 	}
 
 	// Output HTML for <scratchblocks> tag
-	public static function sbRenderTag ($input, array $args, Parser $parser, PPFrame $frame) {
+	public static function sb4RenderTag ($input, array $args, Parser $parser, PPFrame $frame) {
 		return '<pre class="sb4blocks">' . htmlspecialchars($input) . '</pre>';
 	}
 
 	// Output HTML for inline <sb> tag
-	public static function sbRenderInlineTag ($input, array $args, Parser $parser, PPFrame $frame) {
+	public static function sb4RenderInlineTag ($input, array $args, Parser $parser, PPFrame $frame) {
 		return '<code class="sb4blocks">' . htmlspecialchars($input) . '</code>';
 	}
 }
--- run_scratchblocks4.js
+++ run_scratchblocks4.js
@@ -1,7 +1,7 @@
 for (const block of document.querySelectorAll("pre.sb4blocks, code.sb4blocks")) {
 	const script = block.innerText;
-	const doc = scratchblocks.parse(script, {languages:["en", mw.config.get("wgScratchBlocks4Lang")], dialect: "scratch3"});
-	const docView = scratchblocks.newView(doc, {style: "scratch3"});
+	const doc = scratchblocks4.parse(script, {languages:["en", mw.config.get("wgScratchBlocks4Lang")], dialect: "scratch3"});
+	const docView = scratchblocks4.newView(doc, {style: "scratch3"});
 	const svg = docView.render();
 	block.innerHTML = "";
 	block.appendChild(svg);
