ba24e2d8a74e4d0227f5b1f3a8036279850c2967
Ken <kenny2minecraft@gmail.com>
2018-12-30 14:37:36 +0800

    Cleanup (#1)
    * Cleanup
    
    + Allow multiple languages, how hard can it be?
    * Better loading
    * Directly use scratchblocks.github.io minfiles
    * Fix remoteExtPath
    * Switch back to old scratchblocks names, naming conflicts are no worry
    - Removed extension.json version, just use git commit ID
    - Require wfLoadExtension, mostly delete ScratchBlocks.php contents for this
    
    * Update README.md
    
    Removed "Known Bugs" section as those are bugs with the JS side

--- run_scratchblocks4.js
+++ run_scratchblocks4.js
@@ -1,12 +1,8 @@
-for (const block of document.querySelectorAll("pre.sb4blocks, code.sb4blocks")) {
-	const script = block.innerText;
-	const doc = scratchblocks4.parse(script, {languages:["en", mw.config.get("wgScratchBlocks4Lang")], dialect: "scratch3"});
-	const docView = scratchblocks4.newView(doc, {style: "scratch3"});
-	const svg = docView.render();
-	block.innerHTML = "";
-	block.appendChild(svg);
-}
-
-for (const blockSVG of document.querySelectorAll("code.sb4blocks.fixme svg")) {
-    blockSVG.height.baseVal.value -= 30;
-}
+$.when(
+	mw.loader.load('https://scratchblocks.github.io/js/scratchblocks-v4-min.js'),
+	mw.loader.load('https://scratchblocks.github.io/js/translations-all-v4-min.js')
+);
+window.addEventListener('load', function(){
+	scratchblocks.renderMatching('pre.blocks', {languages: ['en'].concat(mw.config.get('wgScratchBlocks4Langs')), style: 'scratch3'});
+	scratchblocks.renderMatching('code.blocks', {languages: ['en'].concat(mw.config.get('wgScratchBlocks4Langs')), style: 'scratch3', inline: true});
+});
