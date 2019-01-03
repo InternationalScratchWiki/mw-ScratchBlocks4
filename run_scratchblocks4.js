4c1070cf6a5ea5d622b2f0deb2a6fbdd7a41cb77
Kenny2github <kenny2minecraft@gmail.com>
2019-01-03 19:50:50 +0800

    none of this transform nonsense

--- run_scratchblocks4.js
+++ run_scratchblocks4.js
@@ -5,4 +5,13 @@ $.when(
 window.addEventListener('load', function(){
 	scratchblocks.renderMatching('pre.blocks', {languages: ['en'].concat(mw.config.get('wgScratchBlocks4Langs')), style: 'scratch3'});
 	scratchblocks.renderMatching('code.blocks', {languages: ['en'].concat(mw.config.get('wgScratchBlocks4Langs')), style: 'scratch3', inline: true});
+	var items = document.querySelectorAll('.scratchblocks svg');
+	var item;
+	for (var i = items.length; i--;) {
+		item = items[i];
+		item.viewBox.baseVal.width = item.width.baseVal.value;
+		item.viewBox.baseVal.height = item.height.baseVal.value;
+		item.style.width = item.width.baseVal.value * 0.675;
+		item.style.height = item.height.baseVal.value * 0.675;
+	}
 });
