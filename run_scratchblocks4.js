df43f5ecdcf6af4705b6d50ea0b620cb2d8855a7
Kenny2github <kenny2minecraft@gmail.com>
2019-01-27 08:00:32 +0100

    Understand jQuery properly ._.

--- run_scratchblocks4.js
+++ run_scratchblocks4.js
@@ -1,14 +1,4 @@
-$.when(
-	mw.loader.load('https://scratchblocks.github.io/js/scratchblocks-v3.3-min.js'),
-	mw.loader.load('https://scratchblocks.github.io/js/translations-all-v3.3.js')
-);
-var scratchblocks_rendered = false;
 function run_scratchblocks() {
-	if (scratchblocks_rendered) {
-		window.removeEventListener('focus', run_scratchblocks);
-		window.removeEventListener('load', run_scratchblocks);
-		return;
-	}
 	scratchblocks.renderMatching('pre.blocks', {languages: ['en'].concat(mw.config.get('wgScratchBlocks4Langs')), style: 'scratch3'});
 	scratchblocks.renderMatching('code.blocks', {languages: ['en'].concat(mw.config.get('wgScratchBlocks4Langs')), style: 'scratch3', inline: true});
 	var items = document.querySelectorAll('.scratchblocks svg');
@@ -20,9 +10,7 @@ function run_scratchblocks() {
 		item.width.baseVal.value *= 0.675;
 		item.height.baseVal.value = item.viewBox.baseVal.height * 0.675;
 	}
-	scratchblocks_rendered = true;
-	window.removeEventListener('focus', run_scratchblocks);
-	window.removeEventListener('load', run_scratchblocks);
 }
-window.addEventListener('focus', run_scratchblocks);
-window.addEventListener('load', run_scratchblocks);
+$.getScript('https://scratchblocks.github.io/js/scratchblocks-v3.3-min.js').done(function(){
+	$.getScript('https://scratchblocks.github.io/js/translations-all-v3.3.js').done(run_scratchblocks);
+});
