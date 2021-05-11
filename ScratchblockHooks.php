bc0d091e922ce9d484f4707ad66e5ad275358043
Kenny2github <kenny2minecraft@gmail.com>
2021-05-11 04:35:07 +0100

    Making changes myself ...because a review would get too complicated
    * Re-added sb4Setup but as a helper to sb4RenderTagGeneric
    * Pass $parser into Generic, thence to Setup
    * Setup only adds the module if not already present, to avoid memory
      bloat, especially on pages with many <(sb|scratchblocks)> tags

--- ScratchblockHooks.php
+++ ScratchblockHooks.php
@@ -1,4 +1,6 @@
 <?php
+define('SB4_MODULE_KEY', 'ext.scratchBlocks4');
+
 class Scratchblock4Hook {
 	// Ouput HTML for <scratchblocks> tag
 
@@ -17,7 +19,15 @@ class Scratchblock4Hook {
 		return true;
 	}
 
-	public static function sb4RenderTagGeneric($input, array $args, $tag) {
+	public static function sb4Setup(Parser $parser) {
+		$out = $parser->getOutput();
+		if (!in_array(SB4_MODULE_KEY, $out->getModules())) {
+			$out->addModules(SB4_MODULE_KEY);
+		}
+	}
+
+	public static function sb4RenderTagGeneric($input, array $args, $parser, $tag) {
+		self::sb4Setup($parser);
 		return (
 			'<'
 			. $tag
@@ -33,14 +43,12 @@ class Scratchblock4Hook {
 
 	// Output HTML for <scratchblocks> tag
 	public static function sb4RenderTag ($input, array $args, Parser $parser, PPFrame $frame) {
-		$parser->getOutput()->addModules('ext.scratchBlocks4');
-		return self::sb4RenderTagGeneric($input, $args, 'pre');
+		return self::sb4RenderTagGeneric($input, $args, $parser, 'pre');
 	}
 
 	// Output HTML for inline <sb> tag
 	public static function sb4RenderInlineTag ($input, array $args, Parser $parser, PPFrame $frame) {
-		$parser->getOutput()->addModules('ext.scratchBlocks4');
-		return self::sb4RenderTagGeneric($input, $args, 'code');
+		return self::sb4RenderTagGeneric($input, $args, $parser, 'code');
 	}
 }
 ?>
